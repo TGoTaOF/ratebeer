c89dcd10dca432a694aa148a5b58bc1fbcea5585
mxsampsa <sampsa.maenpaa96@gmail.com>
2018-10-22 19:00:15 +0300

    Viikon 7 tehtävä 1

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
 
   post 'places', to:'places#search'
 
-
+  get 'beerlist', to:'beers#list'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
